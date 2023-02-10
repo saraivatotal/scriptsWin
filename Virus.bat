63e5b4cc2de8c357980dd619e7a4f823877a0f58
saraivatotal <125080552+saraivatotal@users.noreply.github.com>
2023-02-10 15:09:54 -0300

    Add files via upload

---
+++ Virus.bat
@@ -0,0 +1,77 @@
+  @echo off
+  cls
+  :menu
+  cls
+  color 80
+
+  date /t		
+  time /t
+
+echo Computador: %computername%        Usuario: %username%
+                   
+echo            MENU TAREFAS
+echo  ==================================
+echo * 1. Exibir a calculadora          * 
+echo * 2. Fazer Backup                  *
+echo * 3. Chrome com a Nasa         *
+echo * 4. Painel de Controle            *
+echo * 5. Desligar o computador	   *		 
+echo * 6. Sair  			   * 			
+echo  ==================================
+
+set /p opcao= Escolha uma opcao: 
+echo ------------------------------
+if %opcao% equ 1 goto opcao1
+if %opcao% equ 2 goto opcao2
+if %opcao% equ 3 goto opcao3
+if %opcao% equ 4 goto opcao4
+if %opcao% equ 5 goto opcao5
+if %opcao% equ 6 goto opcao6
+if %opcao% equ 7 goto opcao7
+
+:opcao1
+cls
+calc
+echo ==================================
+echo *      Abriu!!!!    *
+echo ==================================
+pause
+goto menu
+
+:opcao2
+cls
+xcopy /T /C C:\Users\Wagner\Documents\*.* C:\Users\Wagner\Desktop
+echo ==================================
+echo *      Backup concluido           *
+echo ==================================
+pause
+goto menu
+
+:opcao3
+cls
+echo ==================================
+echo *     Nasa no ar       *
+echo ==================================
+start chrome.exe https://nasa.gov
+pause
+goto menu
+
+:opcao4
+cls
+control.exe
+pause
+goto menu
+
+:opcao5
+shutdown -i
+
+:opcao6
+cls
+exit
+
+:opcao7
+echo ==============================================
+echo * Opcao Invalida! Escolha outra opcao do menu *
+echo ==============================================
+pause
+goto menu
